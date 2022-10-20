94e4af90a9124890abe4802e96acf185cc0ea766
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2022-10-20 14:37:47 +0700

    DFS

--- algo/DFS.py
+++ algo/DFS.py
@@ -1,15 +1,31 @@
 from algo.Map import *
 
-class DFS:
-    def __init__(self, start, end , matrix):
-        super().__init__(start, end, matrix)
+class DFS(Map):
+    def __init__(self, matrix, startPoint, endPoint):
+        super().__init__(matrix, startPoint, endPoint)
     def solve(self):
         opened, stack = [],[]
         result = dict() 
 
-        result[self.start] = None
-        result.append(self.start)
-        stack.append(self.start)
+        result[self.startPoint] = None
+        opened.append(self.startPoint)
+        stack.append(self.startPoint)
 
         while len(stack) > 0:
+            item = stack[-1]
+            opened.append(item)
+            stack.pop()
+            if item == self.endPoint:
+                break
+            for step in self.move:
+                nextItem = (item[0] + self.move[step][0], item[1]+ self.move[step][1])
+                
+                if self.isInsideMap(nextItem):
+                    if nextItem not in opened:
+                        stack.append(nextItem)
+                        result[nextItem] = item
+
+        return self.getPath(result)
+
             
+
